775108c376b1206105c7dd92e9a84177b2c8e610
MowlandCodes <mowlandgaming@gmail.com>
2025-09-26 01:30:06 +0700

    faridh -> removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -5,13 +5,10 @@ import {
   makeCacheableSignalKeyStore,
   isJidBroadcast,
   Browsers,
-  DisconnectReason,
-  delay,
 } from "baileys";
 import NodeCache from "node-cache";
 import { pino } from "pino";
 import chalk from "chalk";
-import { Boom } from "@hapi/boom";
 import { question, logger, logInfo } from "#utils/logs";
 import { config } from "#utils/config";
 import { handleConnectionUpdate, handleIncomingMessage } from "#utils/handlers";
@@ -29,10 +26,9 @@ export const startBot = async () => {
 
   console.log(
     logInfo(
-      `Using baileys v${version.join(".")} ${
-        isLatest ? chalk.green("(latest)") : chalk.red("(outdated)")
-      }`
-    ) + "\n"
+      `Using baileys v${version.join(".")} ${isLatest ? chalk.green("(latest)") : chalk.red("(outdated)")
+      }`,
+    ) + "\n",
   );
 
   const bot = makeWASocket({
@@ -54,7 +50,7 @@ export const startBot = async () => {
   // Check connection, if not connected ask for phone number
   if (!bot.authState.creds.registered) {
     const phoneNumber = await question(
-      logInfo("Enter your phone number (format: 628xxxxxxxxxxx) => ")
+      logInfo("Enter your phone number (format: 628xxxxxxxxxxx) => "),
     );
 
     setTimeout(async () => {
