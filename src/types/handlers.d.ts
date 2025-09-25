c748e1b6bbec811f066032a2d500136679c9b284
MowlandCodes <mowlandgaming@gmail.com>
2025-09-26 01:22:41 +0700

    faridh -> type fixing

--- src/types/handlers.d.ts
+++ src/types/handlers.d.ts
@@ -2,3 +2,10 @@ import { type WASocket } from "baileys";
 import { type Logger } from "pino";
 
 export type Handler = { bot: WASocket; logger: Logger };
+
+export type CommandHandler = {
+  bot: WASocket;
+  logger: Logger;
+  senderJid: `${string}@s.whatsapp.net` | `${string}@g.us`;
+  text: string;
+};
--- src/types/parser.d.ts
+++ src/types/parser.d.ts
@@ -4,15 +4,13 @@ import { type Logger } from "pino";
 export type CommandParser = {
   bot: WASocket;
   logger: Logger;
-  senderJid: string;
-  groupJid: string;
-  text?: string;
+  senderJid: `${string}@s.whatsapp.net` | `${string}@g.us`;
+  text: string;
 };
 
 export type messageParser = {
   bot: WASocket;
   logger: Logger;
-  senderJid?: string;
-  groupJid?: string;
-  text?: string;
+  senderJid: `${string}@s.whatsapp.net` | `${string}@g.us`;
+  text: string;
 };
